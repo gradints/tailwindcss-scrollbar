462e27c73852870f8083796634dd5d11c8b1f956
DESKTOP-OV1S94F\Christ <christhofernatalius@gmail.com>
2022-03-30 23:47:32 +0700

    fixed bug for darkMode media, added support for prefix

--- src/index.js
+++ src/index.js
@@ -1,48 +1,63 @@
 const plugin = require('tailwindcss/plugin')
 
+const getSize = (options, theme) => {
+  if (options && options.size) { // with plugin options
+    return options.size
+  }
+  return theme('scrollbar.DEFAULT.size', '5px') // with config
+}
+const getStyleTrack = (options, theme) => {
+  const background = '#f1f1f1'
+  const fromConfig = { // with config
+    background,
+    darkBackground: background,
+    ...theme('scrollbar.DEFAULT.track', {}),
+  }
+  const fromOptions = options && options.track ? options.track : {} // with plugin options
+
+  return { ...fromConfig, ...fromOptions }
+}
+const getStyleThumb = (options, theme) => {
+  const background = '#c1c1c1'
+  const fromConfig = { // with config
+    background,
+    darkBackground: background,
+    ...theme('scrollbar.DEFAULT.thumb', {}),
+  }
+  const fromOptions = options && options.thumb ? options.thumb : {} // with plugin options
+
+  return { ...fromConfig, ...fromOptions }
+}
+const getStyleThumbHover = (options, theme) => {
+  const background = '#a8a8a8'
+  const fromConfig = { // with config
+    background,
+    darkBackground: background,
+    ...theme('scrollbar.DEFAULT.hover', {}),
+  }
+  const fromOptions = options && options.hover ? options.hover : {} // with plugin options
+
+  return { ...fromConfig, ...fromOptions }
+}
+
 module.exports = plugin.withOptions(function (options) {
-  const getSize = (theme) => {
-    if (options && options.size) { // with plugin options
-      return options.size
-    }
-    return theme('scrollbar.DEFAULT.size', '5px') // with config
-  }
-  const getStyleTrack = (theme) => {
-    const fromConfig = { // with config
-      background: theme('scrollbar.DEFAULT.track.background', '#f1f1f1'),
-      darkBackground: theme('scrollbar.DEFAULT.track.darkBackground', '#f1f1f1'),
-      ...theme('scrollbar.DEFAULT.track', {}),
-    }
-    const fromOptions = options && options.track ? options.track : {} // with plugin options
+  return function ({ addBase, theme, config, prefix }) {
+    const size = getSize(options, theme)
+    const track = getStyleTrack(options, theme)
+    const thumb = getStyleThumb(options, theme)
+    const hover = getStyleThumbHover(options, theme)
 
-    return { ...fromConfig, ...fromOptions }
-  }
-  const getStyleThumb = (theme) => {
-    const fromConfig = { // with config
-      background: theme('scrollbar.DEFAULT.thumb.background', '#c1c1c1'),
-      darkBackground: theme('scrollbar.DEFAULT.thumb.darkBackground', '#c1c1c1'),
-      ...theme('scrollbar.DEFAULT.thumb', {}),
-    }
-    const fromOptions = options && options.thumb ? options.thumb : {} // with plugin options
-
-    return { ...fromConfig, ...fromOptions }
-  }
-  const getStyleThumbHover = (theme) => {
-    const fromConfig = { // with config
-      background: theme('scrollbar.DEFAULT.hover.background', '#a8a8a8'),
-      darkBackground: theme('scrollbar.DEFAULT.hover.darkBackground', '#a8a8a8'),
-      ...theme('scrollbar.DEFAULT.hover', {}),
-    }
-    const fromOptions = options && options.hover ? options.hover : {} // with plugin options
-
-    return { ...fromConfig, ...fromOptions }
-  }
-
-  return function ({ addBase, theme }) {
-    const size = getSize(theme)
-    const track = getStyleTrack(theme)
-    const thumb = getStyleThumb(theme)
-    const hover = getStyleThumbHover(theme)
+    addBase([
+      {
+        '::-webkit-scrollbar': {
+          width: size,
+          height: size,
+        },
+        '::-webkit-scrollbar-track': track,
+        '::-webkit-scrollbar-thumb': thumb,
+        '::-webkit-scrollbar-thumb:hover': hover,
+      },
+    ])
 
     const dark = {
       '::-webkit-scrollbar-track': {
@@ -67,21 +82,13 @@ module.exports = plugin.withOptions(function (options) {
       },
     }
 
-    addBase([
-      {
-        '::-webkit-scrollbar': {
-          width: size,
-          height: size,
-        },
-        '::-webkit-scrollbar-track': track,
-        '::-webkit-scrollbar-thumb': thumb,
-        '::-webkit-scrollbar-thumb:hover': hover,
-      },
-      {
-        '.dark': dark,
+    if (config('darkMode') === 'media') {
+      addBase([{
         '@media (prefers-color-scheme: dark)': dark,
         '@media (prefers-color-scheme: light)': light,
-      },
-    ])
+      }])
+    } else {
+      addBase([{ [prefix('.dark')]: dark }])
+    }
   }
 })
